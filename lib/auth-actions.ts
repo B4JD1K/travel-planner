28dabe928fa05a2615ad4ee406699384f5f77d3f
B4JD1K <bajdik@yahoo.com>
2025-08-27 19:06:02 +0200

    fix: add "use server" directive to auth-actions.ts

--- lib/auth-actions.ts
+++ lib/auth-actions.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import {signIn, signOut} from "@/auth";
 
 export const login = async () => {
@@ -5,6 +7,5 @@ export const login = async () => {
 }
 
 export const logout = async () => {
-
   await signOut({redirectTo: "/"})
 }
