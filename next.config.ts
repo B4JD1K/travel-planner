93699defcb0377a8e8147b60b2c6b3e6e32f5560
B4JD1K <bajdik@yahoo.com>
2025-08-29 00:53:55 +0200

    feat: add image upload functionality to trip creation form

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images:{
+    remotePatterns:[{
+      hostname:'h5c7kf26ia.ufs.sh',
+    }]
+  }
 };
 
 export default nextConfig;
