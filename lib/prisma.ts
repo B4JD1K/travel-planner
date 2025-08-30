af49a56f9f6cf7b509291b49d5eda892784af6ec
B4JD1K <bajdik@yahoo.com>
2025-08-30 14:45:23 +0200

    feat: enhance TripsPage with trip listing and upcoming trips summary

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,12 @@
 import {PrismaClient} from "@/app/generated/prisma/client";
 
+// Tworzę zmienną globalną dla Prisma, aby unikać wielokrotnego tworzenia instancji
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
+// Jeśli Prisma już istnieje globalnie, użyj go; jeśli nie to stwórz nową instancję PrismaClient
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+// W trybie development zapisujemy Prisma do globalnego obiektu.
+// Dzięki temu przy "hot reloadzie" w Next.js nie tworzy się wiele instancji,
+// co mogłoby powodować błędy i wycieki pamięci.
