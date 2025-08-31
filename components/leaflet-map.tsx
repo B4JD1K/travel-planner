48d0eab2de863d907f495619d0f48ec5db8c896d
B4JD1K <bajdik@yahoo.com>
2025-09-01 00:27:43 +0200

    feat: migrate map component from Google Maps to Leaflet for improved performance and flexibility,

---
+++ components/leaflet-map.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L, { LatLngTuple } from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+  iconUrl: "/leaflet/marker-icon.png",
+  shadowUrl: "/leaflet/marker-shadow.png",
+});
+
+interface MapWithMarkersProps {
+  itineraries: { lat: number; lon: number; locationTitle: string; id: string }[];
+}
+
+export default function MapWithMarkers({ itineraries }: MapWithMarkersProps) {
+  const center: LatLngTuple =
+    itineraries.length > 0 ? [itineraries[0].lat, itineraries[0].lon] : [0, 0];
+
+  return (
+    <MapContainer center={center} zoom={8} scrollWheelZoom style={{ height: "100vh", width: "100%" }}>
+      <TileLayer
+        url={`https://{s}-tiles.locationiq.com/v3/streets/r/{z}/{x}/{y}.png?key=${process.env.NEXT_PUBLIC_LOCATIONIQ_API_KEY}`}
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      />
+      {itineraries.map((location) => (
+        <Marker key={location.id} position={[location.lat, location.lon]}>
+          <Popup>{location.locationTitle}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
--- components/map.tsx
+++ components/map.tsx
@@ -1,80 +1,14 @@
-"use client"
+import dynamic from "next/dynamic";
+import { Location } from "@/app/generated/prisma";
 
-import {Location} from "@/app/generated/prisma";
-import {GoogleMap, Marker, useLoadScript} from "@react-google-maps/api";
+const LeafletMap = dynamic(() => import("./leaflet-map"), {
+  ssr: false,
+});
 
-interface MapProps {
+interface MapPageProps {
   itineraries: Location[];
 }
 
-export default function Map({itineraries}: MapProps) {
-  const {isLoaded, loadError} = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-  })
-
-  if (loadError) return <div>Error loading maps</div>
-  if (!isLoaded) return <div>Loading maps...</div>
-
-  const center = itineraries.length > 0
-    ? {lat: itineraries[0].lat, lng: itineraries[0].lon}
-    : {lat: 0, lng: 0};
-
-  return (
-    <GoogleMap mapContainerStyle={{width: "100%", height: "100%"}} zoom={8} center={center}>
-      {itineraries.map((location, key) => (
-        <Marker
-          key={key}
-          position={{lat: location.lat, lng: location.lon}}
-          title={location.locationTitle}
-        />
-      ))}
-    </GoogleMap>
-  )
+export default function Map({ itineraries }: MapPageProps) {
+  return <LeafletMap itineraries={itineraries} />;
 }
-
-// TODO: MIGRATE TO LocationIQ from Google Maps
-
-//
-// "use client";
-//
-// import {Location} from "@/app/generated/prisma";
-// import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
-// import L from "leaflet";
-// import "leaflet/dist/leaflet.css";
-//
-// // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// // @ts-ignore
-// const iconDefault = L.Icon.Default.prototype as any;
-// delete iconDefault._getIconUrl;
-// L.Icon.Default.mergeOptions({
-//   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-//   iconUrl: require("leaflet/dist/images/marker-icon.png"),
-//   shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-// });
-//
-// interface MapProps {
-//   itineraries: Location[];
-// }
-//
-// export default function Map({itineraries}: MapProps) {
-//   const center =
-//     itineraries.length > 0
-//       ? [itineraries[0].lat, itineraries[0].lon]
-//       : [0, 0];
-//
-//   return (
-//     <div style={{height: "100vh", width: "100%"}}>
-//       <MapContainer center={center} zoom={8} scrollWheelZoom={true} style={{height: "100%", width: "100%"}}>
-//         <TileLayer
-//           url={`https://maps.locationiq.com/v3/tiles/osm/{z}/{x}/{y}.png?key=${process.env.NEXT_PUBLIC_LOCATIONIQ_API_KEY}`}
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//         />
-//         {itineraries.map((location, key) => (
-//           <Marker key={key} position={[location.lat, location.lon]}>
-//             <Popup>{location.locationTitle}</Popup>
-//           </Marker>
-//         ))}
-//       </MapContainer>
-//     </div>
-//   );
-// }
