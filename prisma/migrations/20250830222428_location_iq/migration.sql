333a83757859cdb32ddceb47e6ae7697d5e82f6e
B4JD1K <bajdik@yahoo.com>
2025-08-31 15:32:25 +0200

    feat: update Location model to replace 'lng' with 'lon' for consistency in coordinate naming

---
+++ prisma/migrations/20250830222428_location_iq/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `lon` on the `Location` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."Location" DROP COLUMN "lon";
